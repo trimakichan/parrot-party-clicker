15988a7d2c554ef9ef7af4ed0f5a20e0cb1a3059
Makiko <trimakichan@gmail.com>
2025-11-21 11:44:12 -0800

    completes the requirements.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,48 @@
 // Add javascript code to Party Parrot Clicker such that:
+const state = {
+  clickCount: 0,
+  imgElement: null,
+  h1Element: null,
+  buttonElement: null
+};
+
+console.log(state);
+
+const counter = () => {
+  state.clickCount += 1;
+  console.log(state.clickCount);
+  state.h1Element.textContent = `Clicks: ${state.clickCount}`;
+};
+
+const resetCounter = () => {
+  state.clickCount = 0;
+  state.h1Element.textContent = `Clicks: ${state.clickCount}`
+};
+
+const registerEvents = () => {
+  state.imgElement.addEventListener('click', counter);
+  state.buttonElement.addEventListener('click', resetCounter);
+
+};
+
+const loadControls = () => {
+  state.imgElement= document.getElementsByTagName('img')[0];
+  state.h1Element= document.getElementsByTagName('h1')[0];
+  state.buttonElement= document.getElementById('resetButton');
+  
+};
+
+
+const onLoaded = () => {
+  // steps to carry out when the page has loaded
+  loadControls();
+  registerEvents();
+};
+
+
+// document.addEventListener('DOMContentLoaded', onLoaded);
+onLoaded();
+
 
 // 1. A click on the Party Parrot increases the counter by 1
 // 2. Add a button called ‘reset’ that will reset the
